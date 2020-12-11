886a6b99631ff40d20b709cc01599212a40c920b
Avneet Saluja <avneet@Avneets-MacBook-Pro.local>
2020-12-10 20:44:28 -0600

    added comments and removed waste lines

--- realtime_facial_landmarks.py
+++ realtime_facial_landmarks.py
@@ -1,37 +1,47 @@
+#code tested on Ubuntu 18.04 environment
 from imutils.video import VideoStream
-from imutils import face_utils
-import datetime
 import argparse
 import imutils
 import time
 import dlib
 import cv2
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-p", "--shape-predictor", required=True,
-	help="path to facial landmark predictor")
-args = vars(ap.parse_args())
+#specify file location for pre-trained landmark detector model used
+parser = argparse.ArgumentParser()
+parser.add_argument("-lp", "--landmark-predictor", required=True)
+args = vars(parser.parse_args())
 
-detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor(args["shape_predictor"])
+#dlib's pre-trained face detector library (based on HOG + SVM) 
+face_detector = dlib.get_frontal_face_detector()
+#dlib's pre-trained landmark keypoints predictor (based on gradient boosting)
+landmark_predictor = dlib.shape_predictor(args["landmark_predictor"])
 
-vs = VideoStream(src=0).start()
+#read video stream from webcam
+video = VideoStream(src=0).start()
 time.sleep(2.0)
 
+#indefinite loop
 while True:
-	frame = vs.read()
-	frame = imutils.resize(frame, width=400)
-	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-	rects = detector(gray, 0)
+	#read the current frame
+	curr_frame = video.read()
+	#resize the frame to better detect face
+	resized_frame = imutils.resize(curr_frame, width=400)
+	#change to grayscale image (more efficient for face detection)
+	gray_frame = cv2.cvtColor(resized_frame, cv2.COLOR_BGR2GRAY)
+	#generates all possible bounding boxes around human faces in the image (in my case only one)
+	bounding_boxes = face_detector(gray_frame, 0)
 
-	for rect in rects:
-		shape = predictor(gray, rect)
-		shape = face_utils.shape_to_np(shape)
-		for (x, y) in shape:
-			cv2.circle(frame, (x, y), 1, (0, 0, 255), -1)
+	#loop through all possible bounding box
+	for bounding_box in bounding_boxes:
+		#given a bounding box and current gray frame, dlib's shape_predictor method generates all landmark keypoints
+		landmark_points = landmark_predictor(gray_frame, bounding_box)
+		#label each keypoint as red circle dots to be displayed in real time
+		for (x, y) in landmark_points:
+			# arguments - frame, keypoint_location, radius, color, thickness
+			cv2.circle(resized_frame, (x, y), 1, (0, 0, 255), -1)
 
-	cv2.imshow("Frame", frame)
-	key = cv2.waitKey(1) & 0xFF
+	cv2.imshow("Realtime_Facial_Landmarks_Demo", resized_frame)
+	cv2.waitKey(1)
 
 cv2.destroyAllWindows()
-vs.stop()
+video.stop()
