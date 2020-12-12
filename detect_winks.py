a3f5bd0613ce0d293b636b01be3475971f59f6d9
Avneet Singh Saluja <saluj012@umn.edu>
2020-12-11 19:27:55 -0600

    added comments and removed redundant code

--- detect_winks.py
+++ detect_winks.py
@@ -1,3 +1,4 @@
+#code tested on Ubuntu 18.04 environment
 from scipy.spatial import distance as dist
 from imutils.video import VideoStream
 from imutils import face_utils
@@ -8,67 +9,86 @@ import time
 import dlib
 import cv2
 
-def eye_aspect_ratio(eye):
-	A = dist.euclidean(eye[1], eye[5])
-	B = dist.euclidean(eye[2], eye[4])
-	C = dist.euclidean(eye[0], eye[3])
-	return (A + B) / (2.0 * C)
+#calculate eye aspect ratio (EAR)
+def EAR(eye_keypoints):
+	#calculate euclidean distance between first pair of keypoints along the width of eye
+	width1 = dist.euclidean(eye_keypoints[1], eye_keypoints[5])
+	#calculate euclidean distance between second pair of keypoints along the width of eye
+	width2 = dist.euclidean(eye_keypoints[2], eye_keypoints[4])
+	#calculate euclidean distance between the only pair of keypoints along the length of eye
+	length = dist.euclidean(eye_keypoints[0], eye_keypoints[3])
+	#calculating the aspect ratio (i.e. width/height)
+	#height multiplied by 2 here correponding to 2 estimates of width
+	return (width1 + width2) / (length * 2.0)
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-p", "--shape-predictor", required=True,
-	help="path to facial landmark predictor")
-args = vars(ap.parse_args())
+#EAR threshold
+ear_threshold = 0.18
+#Number of continuous frames with EAR below threshold => eye blink
+seq_frames = 11
 
-EYE_AR_THRESH = 0.18
-EYE_AR_CONSEC_FRAMES = 11
+#count of continuous frames with ear below threshold for left eye
+curr_count_left = 0
+#count of continuous frames with ear below threshold for right eye
+curr_count_right = 0
+#count of overall total number of winks
+total_winks = 0
 
-COUNTER_LEFT = 0
-COUNTER_RIGHT = 0
-TOTAL = 0
 
-detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor(args["shape_predictor"])
+#dlib's pre-trained face detector library (based on HOG + SVM) 
+face_detector = dlib.get_frontal_face_detector()
+#dlib's pre-trained landmark keypoints predictor (based on gradient boosting)
+landmark_predictor = dlib.shape_predictor("./shape_predictor_68_face_landmarks.dat")
 
-(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
-(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+#getting start and end keypoint indexes from the facial landmarks map
+(l_start, l_end) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+(r_start, r_end) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
 
-vs = VideoStream(src=0).start()
+#read video stream from webcam
+video = VideoStream(src=0).start()
 time.sleep(2.0)
 
+#indefinite loop
 while True:
-	frame = vs.read()
-	frame = imutils.resize(frame, width=400)
-	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-	
-	rects = detector(gray, 0)
+	#read the current frame
+	curr_frame = video.read()
+	#resize the frame to better detect face
+	resized_frame = imutils.resize(curr_frame, width=400)
+	#change to grayscale image (more efficient for face detection)
+	gray_frame = cv2.cvtColor(resized_frame, cv2.COLOR_BGR2GRAY)
+	#generates all possible bounding boxes around human faces in the image (in my case only one)
+	bounding_boxes = face_detector(gray_frame, 0)
 
-	for rect in rects:
-		shape = predictor(gray, rect)
-		shape = face_utils.shape_to_np(shape)
+	#loop through all possible bounding box
+	for bounding_box in bounding_boxes:
+		#given a bounding box and current gray frame, dlib's shape_predictor method generates all landmark keypoints
+		landmark_points = landmark_predictor(gray_frame, bounding_box)
+		#convert to numpy array (making it iterable)
+		landmark_points = face_utils.shape_to_np(landmark_points)
 
-		leftEye = shape[lStart:lEnd]
-		rightEye = shape[rStart:rEnd]
-		leftEAR = eye_aspect_ratio(leftEye)
-		rightEAR = eye_aspect_ratio(rightEye)
+		#eye aspect ratio for left eye
+		left_ear = EAR(landmark_points[l_start:l_end])
+		#eye aspect ratio for right eye
+		right_ear = EAR(landmark_points[r_start:r_end])
 
-		if leftEAR <= EYE_AR_THRESH:
-			COUNTER_LEFT += 1
-		elif leftEAR >= EYE_AR_THRESH:
-			if COUNTER_LEFT >= EYE_AR_CONSEC_FRAMES:
-				TOTAL += 1
-			COUNTER_LEFT = 0
+		if left_ear <= ear_threshold: 
+			curr_count_left += 1
+		else:
+			if curr_count_left >= seq_frames: #implies a left eye wink
+				total_winks += 1
+			curr_count_left = 0
 
-		if rightEAR <= EYE_AR_THRESH:
-			COUNTER_RIGHT += 1
-		elif rightEAR >= EYE_AR_THRESH:
-			if COUNTER_RIGHT >= EYE_AR_CONSEC_FRAMES:
-				TOTAL += 1
-			COUNTER_RIGHT = 0
+		#do same for right eye
+		if right_ear <= ear_threshold:
+			curr_count_right += 1
+		else:
+			if curr_count_right >= seq_frames: #implies a right eye wink
+				total_winks += 1
+			curr_count_right = 0
 
-		cv2.putText(frame, "Winks: {}".format(TOTAL), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-		#cv2.putText(frame, "EAR: {:.2f}".format(leftEAR), (300, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+		# arg: (frame, text, bottom left coordinate of text box, font, font scale factor, font color, font thickness)
+		cv2.putText(resized_frame, "Winks: {}".format(total_winks), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
 
-	cv2.imshow("Frame", frame)
+	cv2.imshow("Winks_Detection_Demo", resized_frame)
 	key = cv2.waitKey(1) & 0xFF
 
 cv2.destroyAllWindows()
